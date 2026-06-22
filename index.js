faa1e1d2419cccc5aa84fe0c8ca98b59f4437802
JacobLuber2 <jaluber10@gmail.com>
2026-06-22 15:57:57 -0600

    finish logic to add numbers to output-display element for a single operand

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const outputDisplay = document.getElementById("output-display");
 let expression = "";
 appendBtns.addEventListener("click", (e) => {
   //   console.log(e.target.textContent);
-  if (expression.length < 18) {
-    expression = expression += e.target.textContent;
-    console.log(expression.length);
+  if (expression.length === 0 || Number(e.target.textContent) !== 0) {
+    if (
+      (expression.length < 18 &&
+        Number(e.target.textContent !== 0) &&
+        !isNaN(Number(e.target.textContent))) ||
+      (expression.length > 0 && !isNaN(expression[expression.length - 1]))
+    ) {
+      expression = expression += e.target.textContent;
+      console.log(Number(e.target.textContent));
+      if (outputDisplay.textContent === "0") {
+        /* clears initial output display value of 0 
+        so adding the expression contents doesn't rerender it */
+        outputDisplay.textContent = "";
+      }
+      outputDisplay.textContent = expression;
+    }
   }
-  outputDisplay.textContent = expression;
 });
