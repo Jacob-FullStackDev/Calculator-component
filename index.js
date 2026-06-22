b84da3e3314e0e562bd58e7b4b11e9a93e133db5
JacobLuber2 <jaluber10@gmail.com>
2026-06-22 14:57:50 -0600

    fix DOM bug

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
 const appendBtns = document.getElementById("append-btns"); // buttons that contain numbers or operators that go after numbers
+console.log(appendBtns);
 const outputDisplay = document.getElementById("output-display");
+console.log(outputDisplay);
 appendBtns.addEventListener("click", (e) => {
   console.log(e.currentTarget);
 });
