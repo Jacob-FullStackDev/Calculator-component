2e09b6e104b011beed2721fcd29bb2881a099e9e
JacobLuber2 <jaluber10@gmail.com>
2026-07-22 14:16:37 -0600

    add ability to reassign operator

--- index.js
+++ index.js
@@ -7,26 +7,92 @@ let operand = "";
 let operator = "";
 let expression = "";
 let total = 0;
-let lastCharOperator = false; // should block adding another operator
+const operators = ["+", "-", "×", "÷", "^"];
+
+function handleOperations(
+  operand1,
+  operator,
+  operand2 = null,
+  percentage = false,
+) {
+  if (operator === "+" && percentage === false) {
+    outputDisplay.textContent = operand1 + operand2;
+    // historyArr.push([operand1, operator, operand2])
+  }
+  if (operator === "-" && percentage === false) {
+    outputDisplay.textContent = operand1 - operand2;
+    // historyArr.push([operand1, operator, operand2])
+  }
+  if (operator === "×" && percentage === false) {
+    outputDisplay.textContent = operand1 * operand2;
+    // historyArr.push([operand1, operator, operand2])
+  }
+  if (operator === "÷" && percentage === false) {
+    if ((operand1 === operand2) === "0") {
+      outputDisplay.textContent = "Can not divide by 0";
+      return;
+    }
+    outputDisplay.textContent = operand1 / operand2;
+    // historyArr.push([operand1, operator, operand2])
+  }
+  if (operator === "^" && percentage === false) {
+    outputDisplay.textContent = operand1 ** operand2;
+    // historyArr.push([operand1, operator, operand2])
+  }
+  if (operator === "÷" && percentage === false) {
+    outputDisplay.textContent = operand1 + operand2;
+    // historyArr.push([operand1, operator, operand2])
+  }
+  if (operator === "√") {
+    outputDisplay.textContent = Math.sqrt(operand);
+    // historyArr.push([operand1, operator])
+  }
+}
 btns.addEventListener("click", (e) => {
   if (e.target.tagName !== "BUTTON") return; // gaurd clause in case a button element isn't pressed
   if (e.target.classList.contains("btn--clear")) {
     // temporary gaurd clause to avoid CE appearing on the output display, TODO: implement clear functionality
     return;
   }
-  if (outputDisplay)
+  if (e.target.classList.contains("btn--num")) {
+    outputDisplay.textContent === "0"
+      ? (outputDisplay.textContent = e.target.textContent)
+      : (outputDisplay.textContent += e.target.textContent);
+  }
+  if (
+    outputDisplay.textContent !== "0" &&
+    e.target.classList.contains("btn--operator")
+  ) {
     if (
-      e.target.classList.contains("btn--num") &&
-      e.target.textContent !== "0" &&
-      outputDisplay.textContent === "0"
+      !operators.includes(
+        outputDisplay.textContent[outputDisplay.textContent.length - 1],
+      )
     ) {
-      // first number inputted
-      outputDisplay.textContent = e.target.textContent;
-    } else if (outputDisplay.textContent !== "0") {
       outputDisplay.textContent += e.target.textContent;
+    } else {
+      console.log(
+        outputDisplay.textContent.slice(
+          0,
+          outputDisplay.textContent.length - 1,
+        ) + e.target.textContent,
+      );
+      outputDisplay.textContent =
+        outputDisplay.textContent.slice(
+          0,
+          outputDisplay.textContent.length - 1,
+        ) + e.target.textContent;
     }
-  // append number to screen
-  // if last character is operator, don't add another operator
-  // if there are 2 or more operands when the = is pressed, return result on screen
-  // push expression to an array, push array to historyObj
+  }
+  if (e.target.classList.contains("btn--special")) {
+    console.log("reached");
+    if (e.target.id === "clear-btn") {
+      console.log("reached id");
+    }
+  }
 });
+// TODO: if operator is truthy, but there is nothing after it, reassign it, else no more operators
+
+// append number to screen
+// if last character is operator, don't add another operator
+// if there are 2 or more operands when the = is pressed, return result on screen
+// push expression to an array, push array to historyObj
