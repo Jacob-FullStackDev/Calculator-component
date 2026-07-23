c27df9dca56f485da85813c1188677e6933b6164
JacobLuber2 <jaluber10@gmail.com>
2026-07-22 20:16:27 -0600

    work on history feature

--- index.js
+++ index.js
@@ -2,19 +2,27 @@
 // DOM elements
 const btns = document.getElementById("calculator-btns");
 const outputDisplay = document.getElementById("output-display");
+const historySection = document.getElementById("history-section");
+const historyBtn = document.getElementById("history-btn");
 // initial state
 const operators = ["+", "-", "×", "÷", "^"];
-const historyArr = [];
-let operand, operatorCount, operator, expression, operandContainsDecimal;
+const historyArr = [["1", "-", "3", "5"]];
+let operand,
+  operatorCount,
+  operator,
+  expression,
+  operandContainsDecimal,
+  squareRoot;
 function init() {
   operand = "";
   operatorCount = 0;
   operator = "";
   expression = [];
   operandContainsDecimal = false;
+  squareRoot = false; // flag if the square root operator is being used
 }
 init();
-function handleOperations(operand1, operator, operand2 = null) {
+function handleOperations(operand1, operator, operand2) {
   if (operator === "+") {
     outputDisplay.textContent = operand1 + operand2;
     historyArr.push([
@@ -40,26 +48,23 @@ function handleOperations(operand1, operator, operand2 = null) {
     outputDisplay.textContent = operand1 / operand2;
     historyArr.push([operand1, operator, operand2, outputDisplay.textContent]);
   }
-  if (operator === "^" && percentage === false) {
+  if (operator === "^") {
     outputDisplay.textContent = operand1 ** operand2;
     historyArr.push([operand1, operator, operand2, outputDisplay.textContent]);
   }
-  if (operator === "÷" && percentage === false) {
+  if (operator === "÷") {
     outputDisplay.textContent = operand1 + operand2;
     historyArr.push([operand1, operator, operand2, outputDisplay.textContent]);
   }
   if (operator === "√") {
-    outputDisplay.textContent = Math.sqrt(operand);
+    outputDisplay.textContent = Math.sqrt(operand1);
     historyArr.push([operand1, operator, outputDisplay.textContent]);
   }
 }
 btns.addEventListener("click", (e) => {
   if (e.target.tagName !== "BUTTON") return; // gaurd clause in case a button element isn't pressed
   // handle numbers
-  if (
-    e.target.classList.contains("btn--num") &&
-    operandContainsPercentage === false
-  ) {
+  if (e.target.classList.contains("btn--num")) {
     outputDisplay.textContent === "0"
       ? (outputDisplay.textContent = e.target.textContent)
       : (outputDisplay.textContent += e.target.textContent);
@@ -68,14 +73,14 @@ btns.addEventListener("click", (e) => {
   if (
     outputDisplay.textContent !== "0" &&
     e.target.classList.contains("btn--operator") &&
-    operatorCount === 0
+    operatorCount === 0 &&
+    operator !== "√"
   ) {
     operator = e.target.textContent;
     expression.push(Number(outputDisplay.textContent), operator);
     outputDisplay.textContent += operator;
     operatorCount++;
     operandContainsDecimal = false;
-    operandContainsPercentage = false;
   }
   if (
     e.target.classList.contains("btn--operator") &&
@@ -88,7 +93,6 @@ btns.addEventListener("click", (e) => {
       outputDisplay.textContent.slice(0, outputDisplay.textContent.length - 1) +
       operator;
     expression[1] = operator;
-    console.log(expression);
   }
   // handle clear, percentages, decimals
   if (e.target.classList.contains("btn--special")) {
@@ -104,9 +108,12 @@ btns.addEventListener("click", (e) => {
           ),
         ),
       );
-      if (expression.includes("√")) {
+      if (squareRoot === false) {
         // all operations besides square root
         handleOperations(expression[0], operator, expression[2]);
+        init();
+      } else {
+        handleOperations(expression[0], operator);
         init();
       }
     }
@@ -114,5 +121,30 @@ btns.addEventListener("click", (e) => {
       outputDisplay.textContent += ".";
       operandContainsDecimal = true;
     }
+    if (e.target.id === "squareroot-btn" && outputDisplay.textContent === "0") {
+      outputDisplay.textContent = "√";
+      console.log(outputDisplay.textContent);
+      operator = "√";
+      squareRoot = true;
+    }
+  }
+});
+historyBtn.addEventListener("click", (e) => {
+  if (historyArr.length > 0) {
+    historySection.classList.toggle("hidden");
+    for (let i = 0; i < historyArr.length; i++) {
+      let historyArrEntry = historyArr[i];
+      let historyArrEntryBtn = document.createElement("button");
+      historySection.append(historyArrEntryBtn);
+      for (let j = 0; j < historyArrEntry.length - 1; j++) {
+        historyArrEntryBtn.textContent += ` ${historyArrEntry[j]}`;
+        historyArrEntryBtn.addEventListener("click", (e) => {
+          outputDisplay.textContent =
+            historyArrEntry[historyArrEntry.length - 1];
+        });
+      }
+    }
+  } else {
+    console.warn("Warning: empty history");
   }
 });
