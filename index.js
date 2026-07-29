3aef6d20c00fa591a6540f4f7601d353c3781133
JacobLuber2 <jaluber10@gmail.com>
2026-07-28 19:33:10 -0600

    finish responsive design, fix color contrast issue

--- index.js
+++ index.js
@@ -96,138 +96,149 @@ function handleOperations(operand1, operator, operand2) {
 btns.addEventListener("click", (e) => {
   if (e.target.tagName !== "BUTTON") return; // gaurd clause in case a button element isn't pressed
   // handle numbers
-  if (e.target.classList.contains("btn--num")) {
-    outputDisplay.textContent === "0"
-      ? (outputDisplay.textContent = e.target.textContent)
-      : (outputDisplay.textContent += e.target.textContent);
-    lastCharDecimal = false;
-  }
-  if (
-    e.target.classList.contains("btn--num") &&
-    e.target.textContent === "0" &&
-    outputDisplay.textContent === "0" &&
-    operandContainsDecimal === false
-  ) {
-    console.warn(
-      "Can not add more than one 0 without another number, select another number",
-    );
-  }
-  // handle addition, subtraction, multiplication, division, exponentiation, all operations in operators array
-  if (
-    outputDisplay.textContent !== "0" &&
-    e.target.classList.contains("btn--operator") &&
-    operatorCount === 0 &&
-    operator !== "√" &&
-    lastCharDecimal === false
-  ) {
-    operator = e.target.textContent;
-    expression.push(Number(outputDisplay.textContent), operator);
-    outputDisplay.textContent += operator;
-    operandContainsDecimal = lastCharDecimal = false;
-    operatorCount++;
-  } else if (
-    operatorCount >= 1 &&
-    e.target.classList.contains("btn--operator")
-  ) {
-    console.warn("Can not add more than 1 operator");
-  } else if (
-    lastCharDecimal === true &&
-    e.target.classList.contains("btn--operator")
-  ) {
-    outputDisplay.textContent =
-      outputDisplay.textContent.slice(0, outputDisplay.textContent.length - 1) +
-      e.target.textContent;
-    operator = e.target.textContent;
-    operatorCount++;
-    operandContainsDecimal = lastCharDecimal = false;
-    expression.push(
-      Number(
+  if (outputDisplay.textContent.length <= 17) {
+    if (e.target.classList.contains("btn--num")) {
+      outputDisplay.textContent === "0"
+        ? (outputDisplay.textContent = e.target.textContent)
+        : (outputDisplay.textContent += e.target.textContent);
+      lastCharDecimal = false;
+    }
+    if (
+      e.target.classList.contains("btn--num") &&
+      e.target.textContent === "0" &&
+      outputDisplay.textContent === "0" &&
+      operandContainsDecimal === false
+    ) {
+      console.warn(
+        "Can not add more than one 0 without another number, select another number",
+      );
+    }
+    // handle addition, subtraction, multiplication, division, exponentiation, all operations in operators array
+    if (
+      outputDisplay.textContent !== "0" &&
+      e.target.classList.contains("btn--operator") &&
+      operatorCount === 0 &&
+      operator !== "√" &&
+      lastCharDecimal === false
+    ) {
+      operator = e.target.textContent;
+      expression.push(Number(outputDisplay.textContent), operator);
+      outputDisplay.textContent += operator;
+      operandContainsDecimal = lastCharDecimal = false;
+      operatorCount++;
+    } else if (
+      operatorCount >= 1 &&
+      e.target.classList.contains("btn--operator")
+    ) {
+      console.warn("Can not add more than 1 operator");
+    } else if (
+      lastCharDecimal === true &&
+      e.target.classList.contains("btn--operator")
+    ) {
+      outputDisplay.textContent =
         outputDisplay.textContent.slice(
           0,
           outputDisplay.textContent.length - 1,
-        ),
-      ),
-      operator,
-    );
-    console.warn(
-      "No number after decimal point, reassigned decimal to operator",
-    );
-  } else if (
-    outputDisplay.textContent === "0" &&
-    e.target.classList.contains("btn--operator")
-  ) {
-    console.warn("Did not add operator since there was no first operand");
-  }
-  if (
-    e.target.classList.contains("btn--operator") &&
-    operators.includes(
-      outputDisplay.textContent[outputDisplay.textContent.length - 1],
-    )
-  ) {
-    operator = e.target.textContent;
-    outputDisplay.textContent =
-      outputDisplay.textContent.slice(0, outputDisplay.textContent.length - 1) +
-      operator;
-    expression[1] = operator;
-  }
-  // handle clear, percentages, decimals
-  if (e.target.classList.contains("btn--special")) {
-    if (e.target.id === "clear-btn") {
-      init(true);
-    }
-    if (e.target.id === "equals-btn") {
-      if (
-        operatorCount > 0 &&
-        !operators.includes(
-          outputDisplay.textContent[outputDisplay.textContent.length - 1],
-        )
-      ) {
-        expression.push(
-          Number(
-            outputDisplay.textContent.slice(
-              outputDisplay.textContent.indexOf(operator) + 1,
-            ),
+        ) + e.target.textContent;
+      operator = e.target.textContent;
+      operatorCount++;
+      operandContainsDecimal = lastCharDecimal = false;
+      expression.push(
+        Number(
+          outputDisplay.textContent.slice(
+            0,
+            outputDisplay.textContent.length - 1,
           ),
-        );
-        if (squareRoot === false) {
-          // all operations besides square root
-          handleOperations(expression[0], operator, expression[2]);
-        } else {
-          handleOperations(expression[0], operator);
+        ),
+        operator,
+      );
+      console.warn(
+        "No number after decimal point, reassigned decimal to operator",
+      );
+    } else if (
+      outputDisplay.textContent === "0" &&
+      e.target.classList.contains("btn--operator")
+    ) {
+      console.warn("Did not add operator since there was no first operand");
+    }
+    if (
+      e.target.classList.contains("btn--operator") &&
+      operators.includes(
+        outputDisplay.textContent[outputDisplay.textContent.length - 1],
+      )
+    ) {
+      operator = e.target.textContent;
+      outputDisplay.textContent =
+        outputDisplay.textContent.slice(
+          0,
+          outputDisplay.textContent.length - 1,
+        ) + operator;
+      expression[1] = operator;
+    }
+    // handle clear, percentages, decimals
+    if (e.target.classList.contains("btn--special")) {
+      if (e.target.id === "clear-btn") {
+        init(true);
+      }
+      if (e.target.id === "equals-btn") {
+        if (
+          operatorCount > 0 &&
+          !operators.includes(
+            outputDisplay.textContent[outputDisplay.textContent.length - 1],
+          )
+        ) {
+          expression.push(
+            Number(
+              outputDisplay.textContent.slice(
+                outputDisplay.textContent.indexOf(operator) + 1,
+              ),
+            ),
+          );
+          if (squareRoot === false) {
+            // all operations besides square root
+            handleOperations(expression[0], operator, expression[2]);
+          } else {
+            handleOperations(expression[0], operator);
+          }
+          return;
         }
-        return;
+        if (
+          operators.includes(
+            outputDisplay.textContent[outputDisplay.textContent.length - 1],
+          )
+        ) {
+          console.warn("No number after operator");
+          return;
+        }
+        if (e.target.id === "equals-btn" && operatorCount === 0) {
+          console.warn("Invalid expression entered");
+        }
+      }
+      if (e.target.id === "decimal-btn" && operandContainsDecimal === false) {
+        outputDisplay.textContent += ".";
+        operandContainsDecimal = lastCharDecimal = true;
+      } else if (
+        e.target.id === "decimal-btn" &&
+        operandContainsDecimal === true
+      ) {
+        console.warn("There can only be 1 active decimal per operand");
       }
       if (
-        operators.includes(
-          outputDisplay.textContent[outputDisplay.textContent.length - 1],
-        )
+        e.target.id === "squareroot-btn" &&
+        outputDisplay.textContent === "0"
       ) {
-        console.warn("No number after operator");
-        return;
+        outputDisplay.textContent = "√";
+        operator = "√";
+        squareRoot = true;
+      } else if (
+        e.target.id === "squareroot-btn" &&
+        outputDisplay.textContent !== "0"
+      ) {
+        console.warn("Square root must be used before first operand");
       }
-      if (e.target.id === "equals-btn" && operatorCount === 0) {
-        console.warn("Invalid expression entered");
-      }
     }
-    if (e.target.id === "decimal-btn" && operandContainsDecimal === false) {
-      outputDisplay.textContent += ".";
-      operandContainsDecimal = lastCharDecimal = true;
-    } else if (
-      e.target.id === "decimal-btn" &&
-      operandContainsDecimal === true
-    ) {
-      console.warn("There can only be 1 active decimal per operand");
-    }
-    if (e.target.id === "squareroot-btn" && outputDisplay.textContent === "0") {
-      outputDisplay.textContent = "√";
-      operator = "√";
-      squareRoot = true;
-    } else if (
-      e.target.id === "squareroot-btn" &&
-      outputDisplay.textContent !== "0"
-    ) {
-      console.warn("Square root must be used before first operand");
-    }
+  } else {
+    console.warn("The character limit is 18");
   }
 });
 historyBtn.addEventListener("click", (e) => {
